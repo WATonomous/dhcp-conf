5327deb9976052ae20bd2d9f53220b1152508833
root <root@derek1-alpine1.129.97.2.1>
2021-06-08 23:51:16 +0000

    Add starting service at boot to start script

--- start-cluster-dhcp.sh
+++ start-cluster-dhcp.sh
@@ -7,12 +7,18 @@ if ! command -v docker &> /dev/null; then
 	apk add docker
 fi
 
-if ! service docker status | grep started; then
+if ! service docker status | grep started &> /dev/null; then
 	echo "------ starting docker service"
 	service docker start
 	sleep 3
 fi
 
+if ! rc-update | grep docker &> /dev/null; then
+	echo "------ adding docker to startup services list"
+	rc-update add docker boot
+fi
+
 echo "------ starting $TYPE DHCP server"
 
-docker run -it --rm --init --net host -v "$(pwd)/data-$TYPE":/data -v "$(pwd)/data-shared":/shared networkboot/dhcpd eth1
+docker run -d --restart always --name dhcp --init --net host -v "$(pwd)/data-$TYPE":/data -v "$(pwd)/data-shared":/shared networkboot/dhcpd eth1
+
